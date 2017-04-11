5d761b70706f3cc66978bb4d04ed39e62966281f
Simon Chuu <simonorj@outlook.com>
2017-04-10 20:30:22 -0400

    Add colors for user groups

--- src/main/java/com/arcaneminecraft/ColorPalette.java
+++ src/main/java/com/arcaneminecraft/ColorPalette.java
@@ -8,6 +8,10 @@ public class ColorPalette {
 	public static final ChatColor FOCUS = ChatColor.WHITE;
 	public static final ChatColor NEGATIVE = ChatColor.RED;
 	public static final ChatColor POSITIVE = ChatColor.GREEN;
+	public static final ChatColor MAP = ChatColor.DARK_GREEN;
+	public static final ChatColor DONOR = ChatColor.DARK_AQUA;
+	public static final ChatColor MOD = ChatColor.BLUE;
+	public static final ChatColor ADMIN = ChatColor.RED;
 	public static final ChatColor RESET = ChatColor.RESET;
 	public static final ChatColor META = ChatColor.YELLOW;
 }
--- src/main/java/com/arcaneminecraft/survival/ArcaneSurvival.java
+++ src/main/java/com/arcaneminecraft/survival/ArcaneSurvival.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
