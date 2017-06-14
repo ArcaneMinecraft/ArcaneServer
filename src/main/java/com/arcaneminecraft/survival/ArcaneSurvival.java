b672c8ee9f09826b61b3a0e407f53dd657d7f9bf
Simon Chuu <simonorj@outlook.com>
2017-06-14 02:24:48 -0400

    Version bump; redact commented out stuff
    ... and move them to SpigotTesting

--- src/main/java/com/arcaneminecraft/survival/ArcaneSurvival.java
+++ src/main/java/com/arcaneminecraft/survival/ArcaneSurvival.java
@@ -191,49 +191,6 @@ public final class ArcaneSurvival extends JavaPlugin {
 			sender.sendMessage(ColorPalette.CONTENT + r);
 			return true;
 		}
-		/*
-		// TODO: what?
-		// Real TODO: move this over to SpigotTesting
-		if (cmd.getName().equalsIgnoreCase("f") & sender.hasPermission("arcane.f")) {
-
-			Player p1 = (Player) sender;
-
-			Firework fw = (Firework) p1.getWorld().spawn(p1.getLocation(), Firework.class);
-			FireworkEffect effect = FireworkEffect.builder().trail(true).flicker(false).withColor(Color.RED)
-					.with(Type.BURST).build();
-			FireworkMeta fwm = fw.getFireworkMeta();
-			fwm.clearEffects();
-			fwm.addEffects(effect);
-
-			@SuppressWarnings("unused")
-			Field f;
-
-			try {
-				f = fwm.getClass().getDeclaredField("power");
-			} catch (NoSuchFieldException e) {
-				e.printStackTrace();
-
-			}
-
-			fw.setFireworkMeta(fwm);
-			return true;
-
-		}
-
-		// TODO: This too goes to SpigotTesting
-		if (cmd.getName().equalsIgnoreCase("doge")) {
-			if (!(sender instanceof Player)) {
-				sender.sendMessage("operatordogedogedogedoge...");
-				return true;
-			}
-
-			if (sender.isOp()) {
-				sender.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "dogecoins iz teh reals " + ((Player)sender).getExhaustion());
-				return true;
-			}
-			return false;
-		}
-		*/
 		return false;
 	}
 }
